9436be286a58c000941d3d8d2e0e486c2e0e4657
André Karrlein <andre@karrlein.com>
2021-09-12 13:07:21 +0200

    add images

--- app/home.go
+++ app/home.go
@@ -19,7 +19,7 @@ func (home *home) Render() app.UI {
 						app.P().Text("auf meiner Website kreatiVroni.de. Ich freue mich sehr, dass du hier gelandet bist und dir meine kreativen Projekte ansiehst. Ich habe es im August 2021 endlich gewagt und einen kleinen Onlineshop eröffnet. Egal, ob ein Einkauf in meinem Shop, ein Feedback zur Website oder ein Herz auf Instagram, ich bin dir für jede Unterstützung sehr dankbar!"),
 					),
 					app.Div().Class("img-box").Body(
-						app.Img().Src("/web/images/logo_192.png"),
+						app.Img().Src("https://storage.googleapis.com/kreativroni-web/Haaand.PNG"),
 					),
 				),
 			),
