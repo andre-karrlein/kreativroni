07104ba82db5f78dfbb63fd65ed2858c577a89f6
André Karrlein <andre.karrlein@karrlein.com>
2022-01-31 19:22:15 +0000

    add quantity to order

--- app/order.go
+++ app/order.go
@@ -7,6 +7,7 @@ type order struct {
 	ProductName  string `json:"productName" firestore:"productName,omitempty"`
 	ProductId    string `json:"productId" firestore:"productId,omitempty"`
 	Price        string `json:"price" firestore:"price,omitempty"`
+	Quantity     string `json:"quantity" firestore:"quantity,omitempty"`
 	Status       string `json:"status" firestore:"status,omitempty"`
 	Email        string `json:"email" firestore:"email,omitempty"`
 	Name         string `json:"name" firestore:"name,omitempty"`
