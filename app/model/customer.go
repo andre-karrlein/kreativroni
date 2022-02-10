e4a1ad88ba11083baff0b61680bb08f27b038af9
André Karrlein <andre.karrlein@karrlein.com>
2022-02-10 19:01:56 +0000

    add customer api endpoint

---
+++ app/model/customer.go
@@ -0,0 +1,8 @@
+package model
+
+type Customer struct {
+	ID             string `json:"id" firestore:"id,omitempty"`
+	CustomerNumber string `json:"customerNumber" firestore:"customerNumber,omitempty"`
+	Name           string `json:"name" firestore:"name,omitempty"`
+	Surname        string `json:"surname" firestore:"surname,omitempty"`
+}
