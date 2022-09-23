61771996ea987ead758fe715339b209a41129e43
André Karrlein <andre@karrlein.com>
2022-09-23 12:47:57 +0200

    Update fashion.go

--- app/fashion.go
+++ app/fashion.go
@@ -8,8 +8,9 @@ type fashion struct {
 	app.Compo
 }
 
-func (fashion *fashion) OnPreRender(ctx app.Context) {
+func (fashion *fashion) OnMount(ctx app.Context) {
 	ctx.Navigate("https://www.spreadshirt.de/shop/user/kreativroni+-+kreatives+von+vroni")
+	ctx.Navigate("/")
 }
 
 func (fashion *fashion) Render() app.UI {
